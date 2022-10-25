c9132c6714ed5b8cc6eb400e3a500b5d1b99e99e
Arman Barraghi <abarraghi@gmail.com>
2022-10-25 15:57:17 +0800

    Laid the foundation for the basic logic for finding a time based only on a single field

--- src/com/github/abarraghi/ClockTester.java
+++ src/com/github/abarraghi/ClockTester.java
@@ -27,148 +27,216 @@ public class ClockTester {
 		};
 		
 		Set<String> availableIds = ZoneId.getAvailableZoneIds();
-		RegionInterpreter regionTime;
-		ShortIDInterpreter shortIDTime;
 		
 		String currString = " ";
 		int i = 0;
 		while(!currString.equals("X")) {
 			currString = testInput[i];
-			String[] timeParams = currString.split("/"); 
 			System.out.println("JClockCLI Test 1 - Iteration: " + i + ", input string: " + currString);
-			
-			switch(timeParams.length) {
-			
-				case 1:
-					if(TimeZoneCollection.getAvailableRegions().contains(timeParams[0])) {
-						
-						regionTime = new RegionInterpreter(timeParams[0]);
-						System.out.println(regionTime.toString());
-						System.out.println(regionTime.getTime());
-						System.out.println("");
-						
-					}
-					
-					else if(TimeZoneCollection.getAvailableShortIDs().contains(timeParams[0])) {
-						
-						shortIDTime = new ShortIDInterpreter(timeParams[0]);
-						System.out.print(shortIDTime.toString());
-						System.out.print(shortIDTime.getTime());
-						System.out.println("");
-						
-						
-					}
-					
-					else {
-						
-						System.out.println("Entered field does not exist!");
-						System.out.println("");
-					}
-					
-					break;
-				
-				case 2:
-					
-					if(TimeZoneCollection.getRegionCityPairings().containsKey(timeParams[0])) {
-						
-//						System.out.println(TimeZoneCollection.getRegionCityPairings().get(timeParams[0]));
-						
-							if(TimeZoneCollection.getRegionCityPairings().get(timeParams[0]).contains(timeParams[1])) {
-								
-								regionTime = new RegionInterpreter(timeParams[0],timeParams[1]);
-								System.out.println(regionTime.toString());
-								System.out.println(regionTime.getTime());
-								System.out.println("");
-								
-							}
-							
-							else {
-								
-								System.out.println("Entered city does not exist for this region!");
-								System.out.println("");
-								
-							}
-						
-					}
-					
-					else if(TimeZoneCollection.getSystemShortPairings().containsKey(timeParams[0])) {
-							
-							if(TimeZoneCollection.getSystemShortPairings().get(timeParams[0]).contains(timeParams[1])) {
-								
-								shortIDTime = new ShortIDInterpreter(timeParams[0],timeParams[1]);
-								System.out.println(shortIDTime.toString());
-								System.out.println(shortIDTime.getTime());
-								System.out.println("");
-								
-							}
-							
-							else {
-								
-								System.out.println("Entered short id does not exist for this system!");
-								System.out.println("");
-								
-							}
-						
-					}
-					
-					else {
-						
-						System.out.println("Entered field does not exist!");
-						System.out.println("");
-					}
-					break;
-					
-				case 3:
-					
-					if(TimeZoneCollection.getRegionSubCityPairings().containsKey(timeParams[0])) {
-						
-						if(TimeZoneCollection.getRegionSubCityPairings().get(timeParams[0]).containsKey(timeParams[1])) {
-							
-							if(TimeZoneCollection.getRegionSubCityPairings().get(timeParams[0]).get(timeParams[1]).contains(timeParams[2])) {
-								
-								regionTime = new RegionInterpreter(timeParams[0],timeParams[1],timeParams[2]);
-								System.out.println(regionTime.toString());
-								System.out.println(regionTime.getTime());
-								System.out.println("");
-								
-							}
-							
-							else {
-								
-								System.out.println("Entered city does not exist!");
-								System.out.println("");
-								
-							}
-							
-						}
-						
-						else {
-							
-							System.out.println("Entered sub region does not exist!");
-							System.out.println("");
-							
-						}
-						
-					}
-					
-					else {
-						
-						System.out.println("Entered region does not exist!");
-						System.out.println("");
-						
-					}
-					
-					break;
-				
-				default :
-					
-					//TODO Implement exception handling
-					
-					break;
-			}
+			matchTimeOnId(currString);
 			
 			i++;
 		}
+		
+	}
+	
+	public static boolean matchTimeOnId(String Id) {
+		
+		String[] timeParams = Id.split("/");
+		RegionInterpreter regionTime;
+		ShortIDInterpreter shortIDTime;
+		
+		switch(timeParams.length) {
+		
+		case 1:
+			if(TimeZoneCollection.getAvailableRegions().contains(timeParams[0])) {
+				
+				regionTime = new RegionInterpreter(timeParams[0]);
+				System.out.println(regionTime.toString());
+				System.out.println(regionTime.getTime());
+				System.out.println("");
+				return true;
+				
+			}
+			
+			else if(TimeZoneCollection.getAvailableShortIDs().contains(timeParams[0])) {
+				
+				shortIDTime = new ShortIDInterpreter(timeParams[0]);
+				System.out.print(shortIDTime.toString());
+				System.out.print(shortIDTime.getTime());
+				System.out.println("");
+				return true;
+				
+			}
+			
+			else {
+				
+				System.out.println("Entered field does not exist!");
+				System.out.println("");
+				return false;
+			}
+		
+		case 2:
+			
+			if(TimeZoneCollection.getRegionCityPairings().containsKey(timeParams[0])) {
+				
+					if(TimeZoneCollection.getRegionCityPairings().get(timeParams[0]).contains(timeParams[1])) {
+						
+						regionTime = new RegionInterpreter(timeParams[0],timeParams[1]);
+						System.out.println(regionTime.toString());
+						System.out.println(regionTime.getTime());
+						System.out.println("");
+						return true;
+						
+					}
+					
+					else {
+						
+						System.out.println("Entered city does not exist for this region!");
+						System.out.println("");
+						return false;
+						
+					}
+				
+			}
+			
+			else if(TimeZoneCollection.getSystemShortPairings().containsKey(timeParams[0])) {
+					
+					if(TimeZoneCollection.getSystemShortPairings().get(timeParams[0]).contains(timeParams[1])) {
+						
+						shortIDTime = new ShortIDInterpreter(timeParams[0],timeParams[1]);
+						System.out.println(shortIDTime.toString());
+						System.out.println(shortIDTime.getTime());
+						System.out.println("");
+						return true;
+						
+					}
+					
+					else {
+						
+						System.out.println("Entered short id does not exist for this system!");
+						System.out.println("");
+						return false;
+						
+					}
+				
+			}
+			
+			else {
+				
+				System.out.println("Entered field does not exist!");
+				System.out.println("");
+				return false;
+			}
+			
+		case 3:
+			
+			if(TimeZoneCollection.getRegionSubCityPairings().containsKey(timeParams[0])) {
+				
+				if(TimeZoneCollection.getRegionSubCityPairings().get(timeParams[0]).containsKey(timeParams[1])) {
+					
+					if(TimeZoneCollection.getRegionSubCityPairings().get(timeParams[0]).get(timeParams[1]).contains(timeParams[2])) {
+						
+						regionTime = new RegionInterpreter(timeParams[0],timeParams[1],timeParams[2]);
+						System.out.println(regionTime.toString());
+						System.out.println(regionTime.getTime());
+						System.out.println("");
+						return true;
+						
+					}
+					
+					else {
+						
+						System.out.println("Entered city does not exist!");
+						System.out.println("");
+						return false;
+						
+					}
+					
+				}
+				
+				else {
+					
+					System.out.println("Entered sub region does not exist!");
+					System.out.println("");
+					return false;
+					
+				}
+				
+			}
+			
+			else {
+				
+				System.out.println("Entered region does not exist!");
+				System.out.println("");
+				return false;
+				
+			}
+		
+		default :
+			
+			System.out.println("Enter a time zone id that contains 1 to 3 fields!");
+			System.out.println("");
+			return false;
+	}
+		
+		
+		
+	}
+	
+	public static boolean matchTimeOnField(String field) {
+		
+		boolean timeMatched;
+		
+		//TODO insert logic for set-field comparisons 
+		
+		
+		 if (TimeZoneCollection.getAvailableRegions().contains(field)) {
+			 
+			 return true;
+			 
+		 }
+		 else if(TimeZoneCollection.getAvailableShortIDs().contains(field)) {
+			 
+			 
+			 return true;
+		 }
+		 else {
+			 
+			 TimeZoneCollection.getRegionCityPairings().forEach((regions,cities) -> {
+				 if(cities.contains(field)) {
+					 
+					 //TODO insert logic for map-field comparisons 
+				 }
+			 });
+			 
+			 TimeZoneCollection.getSystemShortPairings().forEach((systemNames,shortIds) -> {
+				 if(shortIds.contains(field)) {
+					 
+					
+				 }
+			 });
+			 
+			 TimeZoneCollection.getShortGmtPairings().forEach((shortIds,gmtOffsets) -> {
+				 
+				 if(gmtOffsets.contains(TimeZoneCollection.offsetOf(field))) {
+					 
+					
+				 }
+			 });
+			 
+			 TimeZoneCollection.getRegionSubCityPairings().forEach((regions,subregions) -> {
+				 
+				 subregions.forEach((subregionsInner,cities) -> {
+					 if(cities.contains(field)) {
+						 
+						
+					 }
+				 });
+			 });
+			  
+			 
+		 }
 		
 	}
 }
